a0d05f5dff8854ee90b6a62692d91b364e7b5c72
macrespo42 <macrespo@student.42.fr>
2026-02-20 15:26:11 +0100

    refacto(clean_archi): init use_cases

---
+++ use_cases/ports/card_repository.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+from domain.entities.card import Card
+
+
+class CardRepository(ABC):
+    @abstractmethod
+    def save(self, card: Card) -> None:
+        pass
+
+    @abstractmethod
+    def get_by_name(self, name: str) -> Card:
+        pass
+
+    @abstractmethod
+    def get_all_available(self) -> list[Card]:
+        pass
+
+    @abstractmethod
+    def remove(self, name: str) -> Card:
+        pass
