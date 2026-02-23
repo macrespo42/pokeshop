ae5f297adea702cc463bcedb0a99561e05fb7e7a
macrespo42 <macrespo@student.42.fr>
2026-02-23 22:15:40 +0100

    refacto(entities): use dataclass and implement buisness rules inside

--- domain/entities/card.py
+++ domain/entities/card.py
@@ -1,56 +1,94 @@
+import dataclasses
 import datetime
-from enum import StrEnum
-from typing import Optional
+from typing import Optional, ClassVar
 
-from pydantic import BaseModel
-
-
-class Rarity(StrEnum):
-    COMMON = "Common"
-    UNCOMMON = "Uncommon"
-    RARE = "Rare"
-    RARE_HOLO = "Rare Holo"
-    ULTRA_RARE = "Ultra Rare"
-    SECRET = "Secret"
+from dataclasses import dataclass
+from dataclasses import field
 
 
-class PhysicalState(StrEnum):
-    MINT = "Mint"
-    NEAR_MINT = "Near Mint"
-    EXCELLENT = "Excellent"
-    PLAYED = "Played"
-    DAMAGED = "Damaged"
+@dataclass(frozen=True)
+class Rarity:
+    _valid: ClassVar[set] = {"common", "uncommon", "rare", "rare_holo", "ultra_rare"}
+
+    value: str
+
+    def __post_init__(self):
+        if self.value not in self._valid:
+            raise ValueError("Invalid Rarity")
 
 
-class Status(StrEnum):
-    AVAILABLE = "Available"
-    RESERVED = "Reserved"
-    SOLD = "Sold"
-    RETIRED = "Retired"
+@dataclass(frozen=True)
+class PhysicalState:
+    _valid: ClassVar[set] = {"mint", "near mint", "excellent", "played", "damaged"}
+
+    value: str
+
+    def __post_init__(self):
+        if self.value not in self._valid:
+            raise ValueError("Invalid Physical State")
 
 
-class Edition(BaseModel):
+@dataclass(frozen=True)
+class Status:
+    _valid: ClassVar[set] = {"available", "reserved", "sold", "retired"}
+    value: str
+
+    def __post_init__(self):
+        if self.value not in self._valid:
+            raise ValueError("Invalid Status")
+
+
+@dataclass(frozen=True)
+class Edition:
     code: str
     name: str
     years: int
 
+    def __post_init__(self):
+        if self.years < 2000:
+            raise ValueError("Invalid Edition Years")
+        if len(self.name) == 0 or len(self.name) > 32 or not self.name.isalpha():
+            raise ValueError("Invalid Edition Name")
 
-class Card(BaseModel):
-    name: str
+
+@dataclass(frozen=True)
+class PokemonType:
+    _valid: ClassVar[set] = {"normal", "fire", "water", "electric", "grass", "ice", "fighting", "poison", "ground",
+                             "flying",
+                             "psychic", "bug", "rock", "ghost", "dragon", "dark", "steel", "fairy"}
+    value: str
+
+    def __post_init__(self):
+        if self.value not in self._valid:
+            raise ValueError("Invalid Pokemon Type")
+
+
+@dataclass(frozen=True)
+class Name:
+    value: str
+
+    def __post_init__(self):
+        if len(self.value) == 0 or len(self.value) > 32 or not self.value.isalpha():
+            raise ValueError("Invalid Name")
+
+
+@dataclass(frozen=True)
+class Card:
+    name: Name
     rarity: Rarity
     edition: Edition
     physical_state: PhysicalState
-    type: str
-    holo: bool = False
+    type: PokemonType
     illustration: Optional[str]
-    created_at: datetime.datetime = datetime.datetime.now()
-    status: Status = Status.AVAILABLE
+    is_holo: bool = False
+    created_at: datetime.datetime = field(default_factory=datetime.datetime.now)
+    status: Status = Status
 
     def make_available(self):
-        if self.status == Status.SOLD:
-            raise ValueError("Can't make a solded carte Available")
-        self.status = Status.AVAILABLE
+        if self.status.value == "sold":
+            raise ValueError("Can't make a sold carte Available")
+        return dataclasses.replace(self, status=Status("available"))
 
     def sell(self):
-        if self.status == Status.SOLD:
-            raise ValueError("Card already solded")
+        if self.status == "sold":
+            raise ValueError("Card already sold")
