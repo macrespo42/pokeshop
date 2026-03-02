17c6f891430f50bd26fc669be2cac8d4434ba257
macrespo42 <macrespo@student.42.fr>
2026-03-02 22:34:39 +0100

    test: write domain tests

---
+++ domain/entities/test_card.py
@@ -0,0 +1,26 @@
+import pytest
+from .card import Card, Name, Rarity, Edition, PhysicalState, PokemonType, Status
+
+
+@pytest.fixture(name="card")
+def card_fixture():
+    return Card(
+        name=Name("Pikachu"),
+        rarity=Rarity(value="rare"),
+        edition=Edition(code="EG4W", name="RED", years=2002),
+        physical_state=PhysicalState(value="mint"),
+        type=PokemonType(value="electric"),
+        status=Status(value="available"),
+        illustration="foo",
+        is_holo=False,
+    )
+
+def test_given_card_when_sold_cannot_be_available(card):
+    with pytest.raises(ValueError):
+        card.sell()
+        card.make_available()
+
+def test_given_card_when_sold_cannot_be_sold_again(card):
+    with pytest.raises(ValueError):
+        card.sell()
+        card.make_available()
