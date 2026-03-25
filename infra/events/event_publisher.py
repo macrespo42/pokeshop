32777069338c0e5d45148452d96a6d2eb8d115a3
macrespo42 <macrespo@student.42.fr>
2026-03-25 16:36:21 +0100

    feat(infra): concrete implementation of event_pusblisher (in memory)

--- infra/events/event_publisher.py
+++ infra/events/event_publisher.py
@@ -1,9 +1,17 @@
+from typing import Callable
+
 from domain.event.event_publisher import Event, IEventPublisher
 
 
 class InMemoryEventPublisher(IEventPublisher):
     def __init__(self):
-        self.events = []
+        self._handlers = {}
+
+    def subscribe(self, event_type: type, handler: Callable):
+        if not self._handlers.get(event_type):
+            self._handlers[event_type] = []
+        self._handlers[event_type].append(handler)
 
     def publish_event(self, event: Event):
-        self.events.append(event)
+        for handler in self._handlers.get(type(event), []):
+            handler(event)
